37f4a546072e5a903228ae3b9119ecdfe717fa1c
Seungjae Moon <anstmdwo98@naver.com>
2022-03-17 12:41:38 +0900

    first commit

---
+++ seungjae.py
@@ -0,0 +1,37 @@
+print('Hello World')  #001
+print("Mary's cosmetics")  #002
+print('신씨가 소리질렀다. "도둑이야".')  #003
+print('"C:/Windows"')
+print("안녕하세요. \n만나서\t\t반갑습니다.")  #005 \t(tab):탭이고  \n(enter):줄바꿈
+
+movie_rank = ["닥터 스트레인지", "스플릿", "럭키"]  #051
+
+movie_rank = ["닥터 스트레인지", "스플릿", "럭키"]  #052
+movie_rank.append("배트맨")
+print(movie_rank)
+
+movie_rank = ["닥터 스트레인지", "스플릿", "럭키", "배트맨"] #053
+movie_rank.insert(1, "슈퍼맨")
+print(movie_rank)
+
+nums = [1,2,3,4,5]  #058 15
+print(sum(nums))
+
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "소시지", "라면", "팥빙수", "김치전"] #059 12
+print(len(cook))  
+
+nums = [1,2,3,4,5] #060 3.0
+average = sum(nums)/len(nums)
+print(average)
+
+print(3 == 5)  #102 False
+
+print(3<5) #103 True
+
+x=4  #104 True
+print(1<x<5) 
+
+print((3==3) and (4 !=3)) #105 True
+
+
+
